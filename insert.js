ff74b27eb94a2acce30656ac2860dce9c0e897f6
unknown <cliburd@CLIBURD-03.USGANDE.local>
2015-10-20 17:06:44 -0400

    infobox added

--- insert.js
+++ insert.js
@@ -44,7 +44,21 @@ function getCategoryNamesID(){
 		var option = "<option value=" + catNameID[name]+">"+name + "</option>";
 		categorySelector.append(option);
 	}
-	
+	//create div to hold the select box and the custom pop up div that provides options based on the selection
+	var usgeContainer = "<div id='usgeContainer'></div>";
+	var infoBox = "<div id='usgeInfoBox'></div>"
+	var leftprop = (($("#Master_Footer_StatusBar").width() - 330)+2) + "px";
+	var topprop = "-" + ($("#Master_Footer_StatusBar").height() + 6) + "px";
+	var positionprop = "relative";
+	var widthprop = "330px";
+	var props = {
+		left: leftprop,
+		top: topprop,
+		position: positionprop,
+		width: widthprop
+	}
+	$('#Master_Footer_StatusBar').append(usgeContainer);
+	$('#usgeContainer').css(props);
 	//create the select2 combo box
 	$('head').append("<script src='//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js'></script>");
 	$('.category_selector').select2({
@@ -52,26 +66,44 @@ function getCategoryNamesID(){
 		allowClear:true
 	});
 	//format it
-	$('.select2').css("left","1303px");
-	$('.select2').css("top","-30px");
+	//$('.select2').css("left","1303px");
+	$('.select2').css("top",topprop);
+	$('.select2').appendTo('#usgeContainer');
 	$('.select2').css("position","absolute");
 	$('.select2').css("width","330px");
 	$('.select2-selection').css("border-color","#3b6e8f");	
+	$(infoBox).appendTo('#usgeContainer');
+	$('#usgeInfoBox').css({
+		border: "1px solid #3b6e8f",
+		width: "327px",
+		height: "200px",
+		position: "absolute",
+		top: "-230px",
+		"box-shadow": "-3px -3px 5px 2px #ccc",
+		"border-bottom": "0px",
+		visibility: "hidden"
+	});
 	
 	//add change event that will trigger the clicking of the category
 	$('.category_selector').on("change",function (e) {
 		var textSelected = $('.category_selector option:selected').text();
-		var valSelected = $('.category_selector option:selected').val();
-		//running the alternate function to avoid the error the occurs when a category is updated.
-		alternateCategorySelector(valSelected);
-		//try {
-			//if this fails we will go go with the alternate method of looping through the list of categories and selecting the correct one by category id.
-			//categoryList[textSelected+valSelected].click();
-		//} catch (error) {
-			//alternateCategorySelector(valSelected);
-		//}
 		
-	})
+		if (textSelected !== "") {
+			var valSelected = $('.category_selector option:selected').val();
+		//running the alternate function to avoid the error the occurs when a category is updated.
+			alternateCategorySelector(valSelected);		
+			toggleInfoBox();
+		}
+		
+	});
+	
+	//this will close the infobox if it is open when the select two drop down is opened
+	$('.category_selector').on("select2:open",function (e){
+		if ($('#usgeInfoBox').css("visibility")==="visible") {
+			$('#usgeInfoBox').css("visibility","hidden");
+		}
+	});
+	
 	}
 	
 }	
@@ -97,5 +129,8 @@ function alternateCategorySelector(catIDnumber){
 	});
 }
 
+function toggleInfoBox(){
+	$('#usgeInfoBox').css("visibility")==="hidden" ? $('#usgeInfoBox').css("visibility","visible") : $('#usgeInfoBox').css("visibility","hidden");
+}
 
 	
